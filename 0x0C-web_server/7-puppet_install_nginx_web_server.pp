ea24017352840c225a7f75e8eccb9eab3cc89d96
Desheye <aarealaafiaboyz@gmail.com>
2024-03-26 11:21:37 -0700

    puppeteer

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,8 +1,46 @@
-#!/usr/bin/env bash
+#!/usr/bin/env puppet
 
-# Installs a Nginx server
+# Setup New Ubuntu server with nginx
 
-'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+exec { 'update system':
+  command => '/usr/bin/apt-get update',
+}
+
+exec { 'install nginx':
+  command => '/usr/bin/apt-get install -y nginx',
+  require => Exec['update system'],
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Exec['install nginx'],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => file,
+  content => "Hello World!\n",
+  require => Service['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => template('nginx/default.erb'),
+  require => File['/var/www/html/index.nginx-debian.html'],
+}
+
+file { '/var/www/html/error_404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page\n",
+  require => Service['nginx'],
+}
+
+service { 'nginx':
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
+
+exec { 'allow nginx on firewall':
+  command => '/usr/sbin/ufw allow 'Nginx HTTP'',
+  require => Service['nginx'],
+}
 
