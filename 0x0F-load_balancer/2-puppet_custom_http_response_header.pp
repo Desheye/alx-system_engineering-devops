2a8e6b363e172b34e3a13fdb54eee2031114220c
Desheye <aarealaafiaboyz@gmail.com>
2024-03-29 10:12:26 -0700

    puppeteer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,11 +1,31 @@
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By <%= @hostname %>;
-    root   /var/www/html;
-    index  index.html index.htm;
+#Provision a new Ubuntu server with Nginx
+#and configure a custom HTTP header
 
-    location / {
-        try_files $uri $uri/ =404;
-    }
+
+exec { 'update system':
+command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+ensure => 'installed',
+require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+content => 'Hello World!'
+}
+
+exec {'redirect_me':
+command => 'sed -i "24i\ rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+provider => 'shell'
+}
+
+exec {'Configure HTTP header':
+command => 'sed -i "25i\ add_header X-Served-By $hostname;" /etc/nginx/sites-available/default',
+provider => 'shell'
+}
+
+service {'nginx':
+ensure => running,
+require => Package['nginx']
 }
