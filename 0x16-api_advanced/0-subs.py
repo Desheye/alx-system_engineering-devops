6f6092ec98f611abdf6327f5b29125a86a0eaf97
Desheye <aarealafiaboyz@gmail.com>
2024-05-08 04:50:15 +0000

    sub reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
 """
-Function to retrieve the number of subscribers for a given subreddit via the Reddit API.
-If the subreddit is invalid, return 0.
+Queries the Reddit API and returns the total number of subscribers 
+for a given subreddit.
+If the subreddit is invalid, returns 0.
 """
-
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Retrieve the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-        
-    Returns:
-        int: The number of subscribers, or 0 if the subreddit is invalid.
-    """
+    """Returns the number of subscribers for a subreddit."""
     try:
-        headers = {'User-Agent': 'My User Agent 1.0'}
-        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            return response.json()['data']['subscribers']
-        else:
-            return 0
-    except Exception as e:
-        print("An error occurred:", e)
+        url = 'https://www.reddit.com/r/'
+        response = requests.get(url + subreddit + "/about.json",
+                                headers={'User-Agent': 'imgn-drgn'}, allow_redirects=False)
+        return response.json()['data']['subscribers']
+    except:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,21 +8,29 @@ the titles of the first 10 hot posts listed for a given subreddit.
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'MyBot/0.0.1'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            posts = data['data']['children']
-            for post in posts:
-                print(post['data']['title'])
+            posts = data.get('data', {}).get('children', [])
+            if not posts:
+                print("No posts found.")
+            else:
+                for post in posts:
+                    print(post['data']['title'])
         else:
-            print("None")
-    except requests.RequestException:
-        print("None")
+            print("Failed to retrieve data from Reddit.")
+    except requests.RequestException as e:
+        print("An error occurred:", e)
 
 
 # Example usage:
-top_ten("python")
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
